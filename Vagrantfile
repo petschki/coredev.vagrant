d99adf95c4f7426a3d7d5511df280ccdb823aa9d
Peter Mathis <peter.mathis@kombinat.at>
2018-07-20 00:34:25 +0200

    more  transform dependencies

--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,7 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("1") do |config|
-    config.vm.box = "trusty64"
+    config.vm.box = "ubuntu/trusty64"
     config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
 
     config.vm.forward_port 8080, 8080
--- manifests/plone.pp
+++ manifests/plone.pp
@@ -18,6 +18,9 @@ class plone {
   package { "libxslt-dev":
     ensure => present,
   }
+  package { "libpcre++-dev":
+    ensure => present,
+  }
 
   # coredev dependencies
   package { "subversion":
@@ -41,6 +44,14 @@ class plone {
   # transform utilities
   # htmlto<xxx>
   package { "poppler-utils":
+    ensure => present,
+  }
+  # wvHtml
+  package { "wv":
+    ensure => present,
+  }
+ # lynx
+  package { "lynx":
     ensure => present,
   }
 
